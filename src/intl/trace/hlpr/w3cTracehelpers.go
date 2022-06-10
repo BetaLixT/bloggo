dd8ec7f2e46b6691e86103b84505771218631442
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-06-10 05:04:18 +0400

    more implementation of w3c trace helper

--- src/intl/trace/hlpr/w3cTracehelpers.go
+++ src/intl/trace/hlpr/w3cTracehelpers.go
@@ -8,7 +8,13 @@ import (
 )
 
 const (
-	INVALID_VALUE_COUNT = "Invalid number of values"
+	INVALID_VALUE_COUNT = "invalid number of values"
+	VERSION_INVALID_LENGTH = "invalid version length"
+	TRACEID_INVALID_LENGTH = "invalid traceid length"
+	PARENTID_INVALID_LENGTH = "invalid parentid length"
+	FLAG_INVALID_LENGTH = "invalid flag length"
+	TRACEID_IS_ZERO = "error traceid value is zero"
+	PARENTID_IS_ZERO = "error parentid value is zero"
 )
 
 
@@ -88,25 +94,51 @@ func ParseTraceparentRaw(
  	if err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("failed to decode version %w", err)
  	}
+ 	if len(vers) != 1 {
+ 		return nil, nil, nil, nil, fmt.Errorf(VERSION_INVALID_LENGTH)	
+ 	}
+
 	tid, err := base64.StdEncoding.DecodeString(values[1])
  	if err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("failed to decode traceId %w", err)
  	}
+	if len(tid) != 16 {
+ 		return nil, nil, nil, nil, fmt.Errorf(TRACEID_INVALID_LENGTH)	
+ 	}
+
 	pid, err := base64.StdEncoding.DecodeString(values[2])
  	if err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("failed to decode parentId %w", err)
  	}
+ 	if len(tid) != 8 {
+ 		return nil, nil, nil, nil, fmt.Errorf(PARENTID_INVALID_LENGTH)	
+ 	}
+
 	flg, err := base64.StdEncoding.DecodeString(values[3])
  	if err != nil {
 		return nil, nil, nil, nil, fmt.Errorf("failed to decode flag %w", err)
  	}
+	if len(flg) != 1 {
+ 		return nil, nil, nil, nil, fmt.Errorf(FLAG_INVALID_LENGTH)	
+ 	}
+
  	return vers, tid, pid, flg, nil
 }
 
-func ValidateTraceId(traceId string) error {
-  return nil
+func ValidateTraceIdValue(traceId []byte) error {
+	for idx := 0; idx < len(traceId); idx++ {
+		if traceId[idx] != 0 {
+			return nil
+		}
+	}
+  return fmt.Errorf(TRACEID_IS_ZERO)
 }
 
-func ValidateParentId(parentId string) error {
-  return nil
+func ValidateParentIdValue(parentId []byte) error {
+  for idx := 0; idx < len(parentId); idx++ {
+		if parentId[idx] != 0 {
+			return nil
+		}
+	}
+  return fmt.Errorf(TRACEID_IS_ZERO)
 }
